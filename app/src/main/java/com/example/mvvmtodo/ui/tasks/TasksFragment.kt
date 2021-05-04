8a253efd71f8c76821c9a765b53b6eac6609ed34
Manish Pingale <manishpingale2005@gmail.com>
2021-05-04 16:29:44 +0530

    Added Search task capability and fixed a bug

--- app/src/main/java/com/example/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,11 +1,16 @@
 package com.example.mvvmtodo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmtodo.R
+import com.example.mvvmtodo.Util.onQueryTextChanged
 import com.example.mvvmtodo.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,5 +38,36 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
             tasksAdapter.submitList(it)
         }
 
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -4,10 +4,18 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.mvvmtodo.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
 
-    val tasks = taskDao.getTasks().asLiveData()
+    val searchQuery = MutableStateFlow("")
+
+    private val taskFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val tasks = taskFlow.asLiveData()
 }
