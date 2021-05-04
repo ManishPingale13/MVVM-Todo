edfa1df37d608bf425adb7cd9baae456ab6495b8
Manish Pingale <manishpingale2005@gmail.com>
2021-05-04 11:39:06 +0530

    Added ListAdapter

--- app/src/main/java/com/example/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -2,11 +2,12 @@ package com.example.mvvmtodo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.mvvmtodo.data.TaskDao
-import javax.inject.Inject
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
 
+    val tasks = taskDao.getTasks().asLiveData()
 }
