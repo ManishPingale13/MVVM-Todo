2945a03b85e571a775a32b10f0b64f9360af66cd
Manish Pingale <manishpingale2005@gmail.com>
2021-05-14 07:42:25 +0530

    Fixed a bug and App is ready

--- app/src/main/java/com/example/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -30,6 +30,7 @@ import kotlinx.coroutines.launch
 class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.onItemClickListener {
 
     private val viewModel: TasksViewModel by viewModels()
+    private lateinit var searchView: SearchView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -136,7 +137,13 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.onItemClic
         inflater.inflate(R.menu.menu_fragment_tasks, menu)
 
         val searchItem = menu.findItem(R.id.action_search)
-        val searchView = searchItem.actionView as SearchView
+        searchView = searchItem.actionView as SearchView
+
+        val pendingQuery = viewModel.searchQuery.value
+        if (pendingQuery != null && pendingQuery.isNotEmpty()) {
+            searchItem.expandActionView()
+            searchView.setQuery(pendingQuery, false)
+        }
 
         searchView.onQueryTextChanged {
             viewModel.searchQuery.value = it
@@ -170,5 +177,10 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.onItemClic
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        searchView.setOnQueryTextListener(null)
     }
 }
